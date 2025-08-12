c042bb56c1fa32542e31934a11a6bc48665f4487
tusha-patel <tusha.b@queueloopsolutions.com>
2025-08-12 14:05:53 +0530

    done

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,7 +1,7 @@
 export const isDev = process.env.NODE_ENV === "development";
 
 
-export const ORIGIN_URL = isDev
+export const ORIGIN_URL = (isDev
     ? process.env.NEXT_PUBLIC_BASE_URL
-    : process.env.NEXT_PUBLIC_APP_URL;
+    : process.env.NEXT_PUBLIC_APP_URL) || "http://localhost:3000";
 
